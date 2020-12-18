5ffa467f69412673f48f67e4dfb2bfbfd3174c19
Jesus Hernández Martínez <jesus_hdezmtz@hotmail.com>
2020-12-17 18:10:30 -0600

    Creating the repository / sidebar
    Repository created / vuetify and vuex added to project / sidebar component added / corresponding icons added

---
+++ weather-app/src/components/BasePrincipalCard.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="full-size">
+    <v-card width="100%" height="100%" color="#31334a" class="text-center background-image ">
+      <v-toolbar dark color="#31334a" flat>
+        <v-text-field
+          hide-details
+          prepend-icon="mdi-magnify"
+          single-line
+          label="Location"
+          
+        ></v-text-field>
+        <v-btn  rounded color="white" class="ml-2 black--text">
+          Buscar
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-crosshairs-gps</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <div class="pt-15">
+
+      </div>
+      <v-img
+        class="mx-auto"
+        src="../assets/icons/044-sunny.svg"
+        width="100"
+        height="100"
+      ></v-img>
+
+      <p class="white--text text-size-xl font">
+        15<span class="grey--text text-size-md font">℃</span></p>
+
+        <p class="white--text text-size-small">Shower</p>
+        <p class="grey--text text-size-xs">Today</p>
+        <p class="grey--text text-size-xs">
+          <v-icon size="18" color="grey">mdi-map-marker</v-icon> México</p>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BasePrincipalCard",
+};
+</script>
+<style scoped>
+@font-face {
+    font-family: Mazzard;
+    src: url("../assets/MazzardH-SemiBold.ttf");
+  }
+.full-size {
+  width: 100%;
+  height: 100%;
+}
+.background-image {
+  background-image: url("../assets/images/background-1.png");
+  background-size: 100%;
+  background-position: bottom;
+}
+.font{
+  font-family: Mazzard;
+}
+.text-size-xl{
+  font-size: 90px;
+}
+.text-size-l{
+  font-size: 70px;
+}
+.text-size-md{
+  font-size: 50px;
+}
+.text-size-small{
+  font-size: 30px;
+}
+.text-size-xs{
+  font-size: 15px;
+}
+</style>
---
+++ weather-app/src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="background full-size">
+    <v-row class="full-size mx-0">
+        <v-col cols="12" xs="12" sm="12" md="4" lg="4" xl="4" class="pa-0 ma-0">
+            <basePrincipalCard></basePrincipalCard>
+        </v-col>
+        <v-col cols="12" xs="12" sm="12" md="8" lg="8" xl="8"  class="pa-0 ma-0">
+            
+        </v-col>
+    </v-row>
+</div>
+</template>
+
+<script>
+import basePrincipalCard from "@/components/BasePrincipalCard.vue";
+
+export default {
+  components: {
+    basePrincipalCard,
+
+  },
+};
+</script>
+
+<style scoped>
+.background {
+  background: linear-gradient(
+    to bottom,
+    #2B2D42 0%,#000000 100%
+  );
+}
+.full-size{
+  width: 100%;
+  height: 100%;
+}
+</style>
